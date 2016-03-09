09e2fa1fa74f898cd064b6881f199ef6db3f57cb
katherdx <katherjmc@gmail.com>
2016-03-09 14:03:35 +0530

    Update createdb.js

--- server/boot/createdb.js
+++ server/boot/createdb.js
@@ -3,8 +3,8 @@ var ds = server.dataSources.MySQL;
  
 
 var lbTables = ['post','share'];
-//ds.automigrate(lbTables, function(er) {
-ds.autoupdate(lbTables, function(er) {
+ds.automigrate(lbTables, function(er) {
+//ds.autoupdate(lbTables, function(er) {
   if (er) throw er;
   console.log('Loopback tables [' + lbTables + '] created in ', ds.adapter.name);
   //ds.disconnect();
